6494383ce25ed19be75e51b5f7737ef99ba06029
Philippe Nikolov <philippe.nikolov@outlook.com>
2025-04-16 14:03:57 -0400

    neotree right toggle

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,5 +1,6 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
+
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
@@ -10,36 +11,7 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    {
-      '\\',
-      function()
-        if vim.bo.filetype == 'neo-tree' then
-          vim.cmd 'Neotree close'
-        else
-          -- Check if Neo-tree is already open
-          local neo_tree_windows = vim.tbl_filter(function(win)
-            local bufnr = vim.api.nvim_win_get_buf(win)
-            return vim.bo[bufnr].filetype == 'neo-tree'
-          end, vim.api.nvim_list_wins())
-
-          if #neo_tree_windows > 0 then
-            vim.cmd 'Neotree close'
-          else
-            vim.cmd 'Neotree reveal'
-          end
-        end
-      end,
-      desc = 'NeoTree toggle',
-      silent = true,
-    },
+    { '\\', ':Neotree toggle right<CR>', desc = 'NeoTree Toggle', silent = true },
   },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
+  opts = {},
 }
